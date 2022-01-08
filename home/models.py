685769dd7f7e29bd314d4eefabcdc2c117dae1e5
monkey <fanghuawfh@gmail.com>
2022-01-08 21:02:59 +0800

    feat：调整Banner封装

--- home/models.py
+++ home/models.py
@@ -1,41 +1,20 @@
 from django.db import models
-from modelcluster.fields import ParentalKey
 
-from wagtail.core.models import Page, Orderable
+from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel, InlinePanel
+from wagtail.admin.edit_handlers import FieldPanel
 from wagtail.snippets.edit_handlers import SnippetChooserPanel
 
 
 class HomePage(Page):
     body = RichTextField(blank=True)
-    metadata = models.ForeignKey('snippets.Metadata',
-                                 null=True,
-                                 blank=True,
-                                 on_delete=models.SET_NULL,
-                                 related_name='+',
-                                 verbose_name='请选择元数据')
+    banner = models.ForeignKey('snippets.Banner',
+                               null=True,
+                               on_delete=models.SET_NULL,
+                               related_name='+',
+                               verbose_name='请选择Banner')
 
     content_panels = Page.content_panels + [
         FieldPanel('body', classname="full"),
-        SnippetChooserPanel('metadata'),
-        InlinePanel('banner', label='请选择banner资源')
+        SnippetChooserPanel('banner')
     ]
-
-
-class HomeBanner(Orderable, models.Model):
-    page = ParentalKey(HomePage,
-                       on_delete=models.SET_NULL,
-                       related_name='banner',
-                       null=True)
-    metadata = models.ForeignKey(
-        'snippets.Metadata',
-        on_delete=models.CASCADE,
-        null=True,
-        related_name='+',
-    )
-
-    panels = [SnippetChooserPanel('metadata')]
-
-    def __str__(self):
-        return self.page.title + '->' + self.metadata.title
