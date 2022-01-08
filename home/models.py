c623b87ec2ded80f290b05332da6272a1e52c290
monkey <fanghuawfh@gmail.com>
2022-01-08 20:49:19 +0800

    feat：抽象出Banner组件

--- home/models.py
+++ home/models.py
@@ -1,23 +1,20 @@
 from django.db import models
-from django.db.models.functions.text import Ord
 from modelcluster.fields import ParentalKey
 
 from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel, InlinePanel
-from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.snippets.edit_handlers import SnippetChooserPanel
+
 
 class HomePage(Page):
     body = RichTextField(blank=True)
-    metadata = models.ForeignKey(
-        'snippets.Metadata',
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name='+',
-        verbose_name='请选择元数据'
-    )
+    metadata = models.ForeignKey('snippets.Metadata',
+                                 null=True,
+                                 blank=True,
+                                 on_delete=models.SET_NULL,
+                                 related_name='+',
+                                 verbose_name='请选择元数据')
 
     content_panels = Page.content_panels + [
         FieldPanel('body', classname="full"),
@@ -25,13 +22,12 @@ class HomePage(Page):
         InlinePanel('banner', label='请选择banner资源')
     ]
 
+
 class HomeBanner(Orderable, models.Model):
-    page = ParentalKey(
-        HomePage,
-        on_delete=models.SET_NULL,
-        related_name='banner',
-        null=True
-        )
+    page = ParentalKey(HomePage,
+                       on_delete=models.SET_NULL,
+                       related_name='banner',
+                       null=True)
     metadata = models.ForeignKey(
         'snippets.Metadata',
         on_delete=models.CASCADE,
@@ -39,10 +35,7 @@ class HomeBanner(Orderable, models.Model):
         related_name='+',
     )
 
-    panels = [
-        SnippetChooserPanel('metadata')
-    ]
+    panels = [SnippetChooserPanel('metadata')]
 
     def __str__(self):
         return self.page.title + '->' + self.metadata.title
-
--- snippets/models.py
+++ snippets/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django import forms
 
 from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.snippets.edit_handlers import SnippetChooserPanel
 from wagtail.snippets.models import register_snippet
 from wagtail.images.edit_handlers import ImageChooserPanel
 
@@ -46,7 +47,12 @@ class Metadata(models.Model):
     image = models.ForeignKey('wagtailimages.Image',
                               on_delete=models.CASCADE,
                               related_name='+',
-                              help_text='请选择元数据对应的图片资源')
+                              help_text='请选择元数据对应的图片资源',
+                              verbose_name='本地图片资源',
+                              blank=True)
+    img_url = models.URLField(help_text='请输入图片url地址',
+                              verbose_name='远程图片资源URL',
+                              blank=True)
 
     def __str__(self):
         return self.title
@@ -57,8 +63,70 @@ class Metadata(models.Model):
     panels = [
         FieldPanel('title'),
         FieldPanel('type', widget=forms.Select),
+        FieldPanel('img_url'),
         ImageChooserPanel('image'),
         FieldPanel('url')
+    ]
+
+
+@register_snippet
+class Banner(models.Model):
+    title = models.CharField(max_length=30,
+                             verbose_name='Banner标题',
+                             help_text='请输入Banner标题')
+    type = models.CharField(max_length=20,
+                            choices=TYPE_DICT,
+                            help_text='请选择元数据适用平台',
+                            default=Type.PC.name)
+    first = models.ForeignKey('Metadata',
+                              on_delete=models.SET_NULL,
+                              related_name='+',
+                              help_text='请选择第一张轮播图元素数据',
+                              null=True,
+                              blank=False)
+    second = models.ForeignKey('Metadata',
+                               on_delete=models.SET_NULL,
+                               related_name='+',
+                               help_text='请选择第二张轮播图元素数据',
+                               null=True,
+                               blank=True)
+    third = models.ForeignKey('Metadata',
+                              on_delete=models.SET_NULL,
+                              related_name='+',
+                              help_text='请选择第三张轮播图元素数据',
+                              null=True,
+                              blank=True)
+    fouth = models.ForeignKey('Metadata',
+                              on_delete=models.SET_NULL,
+                              related_name='+',
+                              help_text='请选择第四张轮播图元素数据',
+                              null=True,
+                              blank=True)
+    fifth = models.ForeignKey('Metadata',
+                              on_delete=models.SET_NULL,
+                              related_name='+',
+                              help_text='请选择第五张轮播图元素数据',
+                              null=True,
+                              blank=True)
+    sixth = models.ForeignKey('Metadata',
+                              on_delete=models.SET_NULL,
+                              related_name='+',
+                              help_text='请选择第六张轮播图元素数据',
+                              null=True,
+                              blank=True)
+
+    def __str__(self):
+        return self.title
+
+    panels = [
+        FieldPanel('title'),
+        FieldPanel('type', widget=forms.Select),
+        SnippetChooserPanel('first'),
+        SnippetChooserPanel('second'),
+        SnippetChooserPanel('third'),
+        SnippetChooserPanel('fouth'),
+        SnippetChooserPanel('fifth'),
+        SnippetChooserPanel('sixth')
     ]
 
 
