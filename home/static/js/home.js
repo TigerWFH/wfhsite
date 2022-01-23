f574fcf4e091f5fcbe9872c3279d782b96d07fe2
monkey <fanghuawfh@gmail.com>
2022-01-23 21:16:54 +0800

    feat：初步实现carousel

--- home/static/js/home.js
+++ home/static/js/home.js
@@ -1,5 +1,33 @@
 // 处理common-banner逻辑
 
-const banner = $('common-banner');
+$('#carousel').click(() => {
+  console.log('click');
+});
 
-console.log('banner===>', banner);
+$('document').ready(() => {
+  console.log(6666666666);
+  $('#stage div').each(function (index) {
+    console.log('index===>', index, this);
+    let cl = 'first';
+    switch (index) {
+      case 0:
+        break;
+      case 1:
+        cl = 'second';
+        break;
+      case 2:
+        cl = 'third';
+        break;
+      case 3:
+        cl = 'fourth';
+        break;
+      case 4:
+        cl = 'fifth';
+        break;
+      case 5:
+        cl = 'sixth';
+        break;
+    }
+    $(this).addClass(cl);
+  });
+});
